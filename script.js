7b004de17dbea2416fdeb22a74fea4029babc818
MananJO09 <87749897+MananJO09@users.noreply.github.com>
2023-05-22 19:01:04 +0530

    created a submission

--- script.js
+++ script.js
@@ -5,13 +5,12 @@ function palindrome(str){
 	for (let i = 0; i < len / 2; i++) {
 
         // check if first and last string are same
-        if (str[i] !== string[len - 1 - i]) {
+        if (str[i] !== str[len - 1 - i]) {
             return 'false';
         }
     }
     return 'true';
 }
-const str = prompt("");
-const value = palindrome(str);
+
 
 module.exports = palindrome
