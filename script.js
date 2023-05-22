10960be0b03eaca10d7e3d3c4c668b55bcb035f5
MananJO09 <87749897+MananJO09@users.noreply.github.com>
2023-05-22 19:10:22 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,18 +1,19 @@
-// complete the given function
+function palindrome(str) {
+    // Convert the input string to lowercase
+    str = str.toLowerCase();
 
-function palindrome(str){
-		str=str.toLowerCase();
-		str=str.toreplace(/[^a-z0-9]/g, '');
-	const len=str.length;
-	for (let i = 0; i < len / 2; i++) {
-		
+    // Remove any non-alphanumeric characters from the string
+    str = str.replace(/[^a-z0-9]/g, '');
+
+    const len = str.length;
+    for (let i = 0; i < len / 2; i++) {
+
         // check if first and last string are same
         if (str[i] !== str[len - 1 - i]) {
-            return 'false';
+            return false;
         }
     }
-    return 'true';
+    return true;
 }
 
-
-module.exports = palindrome
+module.exports = palindrome;
