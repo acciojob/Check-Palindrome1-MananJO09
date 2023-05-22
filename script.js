63fa5d83ad3dced0d9c8a9d5b857e2e435e909a8
MananJO09 <87749897+MananJO09@users.noreply.github.com>
2023-05-22 19:08:42 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,9 +1,11 @@
 // complete the given function
 
 function palindrome(str){
+		str=str.toLowerCase();
+		str=str.toreplace(/[^a-z0-9]/g, '');
 	const len=str.length;
 	for (let i = 0; i < len / 2; i++) {
-
+		
         // check if first and last string are same
         if (str[i] !== str[len - 1 - i]) {
             return 'false';
